fb68c105ea25c276805cbba5c8071e949a0fa891
Jon Rovira <jonathanrovira2016@u.northwestern.edu>
2015-02-28 11:55:01 -0600

    createChart function and callback cleaned up and inserted into controller

--- app/scripts/controllers/controllers.js
+++ app/scripts/controllers/controllers.js
@@ -8,6 +8,44 @@ var setHeights = function() {
     $('main').height(Math.ceil(windowH - headerH - footerH - navH));
 };
 
+var createChart = function(seriesOptions) {
+    $('#line-chart').highcharts('StockChart', {
+        rangeSelector: {
+            selected: 4
+        },
+        xAxis: {
+            type: 'datetime',
+            dateTimeLabelFormats: {
+                day: '%e of %b'
+            }
+        },
+        yAxis: {
+            labels: {
+                formatter: function () {
+                    return (this.value > 0 ? ' + ' : '') + this.value + '%';
+                }
+            },
+            plotLines: [{
+                value: 0,
+                width: 2,
+                color: 'silver'
+            }]
+        },
+        plotOptions: {
+            series: {
+                compare: 'percent'
+            }
+        },
+        tooltip: {
+            pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
+            valueDecimals: 2
+        },
+        series: seriesOptions
+    });
+};
+
+
+
 angular.module('markoApp')
 
     .controller('DrawerCtrl', function ($scope, $http) {
@@ -21,7 +59,6 @@ angular.module('markoApp')
 
         $http.get(url)
             .success(function(data, status, headers, config) {
-    		      console.log(data);
             })
             .error(function(data, status, headers, config) {
     		      console.log('Error occurred, data given was ' + data);
@@ -32,6 +69,30 @@ angular.module('markoApp')
     .controller('PerformanceCtrl', function ($rootScope, $scope, $route) {
         $rootScope.activeTab = $route.current.activeTab;
         setHeights();
+
+        var seriesOptions = [],
+            seriesCounter = 0;
+
+        angular.element(document).ready(function () {
+            var names = ['AAPL', 'GOOGL'];
+
+            $.each(names, function (i, name) {
+                $.getJSON('https://www.quandl.com/api/v1/datasets/WIKI/'+name+'.json?exclude_headers=true&sort_order=asc&column=11&collapse=daily&auth_token=J7hxBtcABx8AsszfDzq-', function (result) {
+                    seriesOptions[i] = {
+                        name: name,
+                        data: result.data.map(function(obj) {
+                            return [ Date.parse(obj[0]), obj[1]];
+                        })
+                    };
+                })
+                .done(function() {
+                    seriesCounter += 1;
+                    if (seriesCounter == names.length) {
+                        createChart(seriesOptions);
+                    }
+                });
+            });
+        });
     })
 
 
