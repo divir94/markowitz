e5ccda1fff27a21efc75f2cbe8814e01c1e9e2c7
Jon Rovira <jonathanrovira2016@u.northwestern.edu>
2015-02-28 01:46:57 -0600

    drawer and nav tabs moved into views, included with ng-include

--- app/scripts/style.js
+++ app/scripts/style.js
@@ -1,11 +1,10 @@
 $(document).ready(function() {
-	var windowH = $(window).outerHeight();
-	var headerH = $('header').outerHeight();
-	var navH    = $('nav.tabs').outerHeight();
-	var footerH = $('footer').outerHeight();
-
-	$('section.drawer').height(Math.floor(windowH - headerH - footerH));
-	setTimeout(function(){
-	    $('main').height(Math.ceil(windowH - headerH - footerH - navH));
+	setTimeout(function() {
+		var windowH = $(window).outerHeight();
+		var headerH = $('header').outerHeight();
+		var navH    = $('nav.tabs').outerHeight();
+		var footerH = $('footer').outerHeight();
+		$('section.drawer').height(Math.floor(windowH - headerH - footerH));
+		$('main').height(Math.ceil(windowH - headerH - footerH - navH));
 	}, 100);
 });
