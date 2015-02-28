b5528c42fc68865aaaf41b3d1dd054da9c783d7f
Jon Rovira <jonathanrovira2016@u.northwestern.edu>
2015-02-28 08:51:38 -0600

    style pushed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,33 +11,35 @@
 
 
 angular
-  .module('markoApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch',
-    'autocomplete'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/performance', {
-        templateUrl: 'views/performance.html',
-        controller: 'PerformanceCtrl',
-        activeTab: 'performance'
-      })
-      .when('/correlation', {
-        templateUrl: 'views/correlation.html',
-        controller: 'CorrelationCtrl',
-        activeTab: 'correlation'
-      })
-      .when('/summary', {
-        templateUrl: 'views/summary.html',
-        controller: 'SummaryCtrl',
-        activeTab: 'summary'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .module('markoApp', [
+        'ngAnimate',
+        'ngCookies',
+        'ngResource',
+        'ngRoute',
+        'ngSanitize',
+        'ngTouch',
+        'autocomplete'
+    ])
+    .config(function ($routeProvider) {
+        $routeProvider
+
+
+            .when('/performance', {
+                templateUrl: 'views/performance.html',
+                controller: 'PerformanceCtrl',
+                activeTab: 'performance'
+            })
+            .when('/correlation', {
+                templateUrl: 'views/correlation.html',
+                controller: 'CorrelationCtrl',
+                activeTab: 'correlation'
+            })
+            .when('/summary', {
+                templateUrl: 'views/summary.html',
+                controller: 'SummaryCtrl',
+                activeTab: 'summary'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });  
