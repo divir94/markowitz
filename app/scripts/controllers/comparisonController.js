ab6b240c1d30c95a84f0239be62783b8abac2d7d
Jon Rovira <jonathanrovira2016@u.northwestern.edu>
2015-03-01 11:10:30 -0600

    loader only displays when user actually inputs information, otherwise sleeping icon shown, added info logo at top right with intent of adding info modal/page

--- app/scripts/controllers/comparisonController.js
+++ app/scripts/controllers/comparisonController.js
@@ -9,27 +9,27 @@ angular.module('markoApp').controller('ComparisonCtrl', function ($rootScope, $s
 
     var addLine = function(item) {
         $http.get('https://sleepy-cove-7513.herokuapp.com/quandl', {
-          params: {
-            stocks: item
+            params: {
+                stocks: item
         }})
         .success(function(data, status, headers, config) {
-                // do nothing if empty array returned
-                if (data.length === 0) {
-                    return;
-                }
-                var stock = data[0];
-                var temp = {
-                  name: item,
-                  color: colors[4],
-                  dataLabels: name,
-                  data: stock.map(function(obj) {
+            // do nothing if empty array returned
+            if (data.length === 0) {
+                return;
+            }
+            var stock = data[0];
+            var temp = {
+                name: item,
+                color: colors[4],
+                dataLabels: name,
+                data: stock.map(function(obj) {
                     return [Date.parse(obj[0]), obj[1]];
                 })
-                };
-              seriesOptions.push(temp);
-              $('section.loader').hide();
-              createChart(seriesOptions,3);
-          })
+            };
+            seriesOptions.push(temp);
+            $('section.loader').hide();
+            createChart(seriesOptions,3);
+        })
         .error(function(data, status, headers, config) {
             console.log('Error from addLine');
         })
@@ -47,31 +47,32 @@ angular.module('markoApp').controller('ComparisonCtrl', function ($rootScope, $s
 
     //https://sleepy-cove-7513.herokuapp.com/portfolio'
     $http.get('http://localhost:5000/strategy', {
-      params: {
-        strategies: $rootScope.strategies
-    }})
-      .success(function(data, status, headers, config) {
-              // do nothing if empty array returned
-              console.log(data);
-              if (data.length === 0) {
-                  return;
-              }
-              var temp = {
-                  name: 'Strategies',
-                  color: colors[1],
-                  dataLabels: name,
-                  data: data.map(function(obj) {
-                      return [Date.parse(obj[0]), parseInt(obj[1])];
-                  })
-              };
-              console.log('add to so');
-            seriesOptions.push(temp);
-            $('section.loader').hide();
-            createChart(seriesOptions);
-      })
-      .error(function(data, status, headers, config) {
-          console.log('Error from addLine');
-      });
+        params: {
+            strategies: $rootScope.strategies
+        }
+    })
+    .success(function(data, status, headers, config) {
+        // do nothing if empty array returned
+        console.log(data);
+        if (data.length === 0) {
+            return;
+        }
+        var temp = {
+            name: 'Strategies',
+            color: colors[1],
+            dataLabels: name,
+            data: data.map(function(obj) {
+                return [Date.parse(obj[0]), parseInt(obj[1])];
+            })
+        };
+        console.log('add to so');
+        seriesOptions.push(temp);
+        $('section.loader').hide();
+        createChart(seriesOptions);
+    })
+    .error(function(data, status, headers, config) {
+        console.log('Error from addLine');
+    });
 
 
     // $http.get('http://localhost:5000/compute/strategy', {
--- app/scripts/controllers/stocksController.js
+++ app/scripts/controllers/stocksController.js
@@ -12,35 +12,36 @@ angular.module('markoApp').controller('StocksPortfolioCtrl', function ($rootScop
     var colors = ['#7cb5ec', '#434348', '#90ed7d', '#f7a35c', '#8085e9', '#f15c80', '#e4d354', '#8085e8', '#8d4653', '#91e8e1'];
 
     var drawExistingLines = function() {
-      for (var i = 0; i < $rootScope.stocks.length; i++) {
-        addLine($rootScope.stocks[i]);
-      }
+        for (var i = 0; i < $rootScope.stocks.length; i++) {
+            addLine($rootScope.stocks[i]);
+        }
     }
 
     var addLine = function(item) {
-      // https://sleepy-cove-7513.herokuapp.com/quandl
+        // https://sleepy-cove-7513.herokuapp.com/quandl
         $http.get('https://sleepy-cove-7513.herokuapp.com/quandl', {
-          params: {
-            stock: item
-        }})
+            params: {
+                stock: item
+            }
+        })
         .success(function(data, status, headers, config) {
-                // do nothing if empty array returned
-                if (data.length === 0) {
-                    return;
-                }
-                var stock = data;
-                var temp = {
-                  name: item,
-                  color: colors[$rootScope.stocks.length],
-                  dataLabels: name,
-                  data: stock.map(function(obj) {
+            // do nothing if empty array returned
+            if (data.length === 0) {
+                return;
+            }
+            var stock = data;
+            var temp = {
+                name: item,
+                color: colors[$rootScope.stocks.length],
+                dataLabels: name,
+                data: stock.map(function(obj) {
                     return [Date.parse(obj[0]), obj[1]];
                 })
-                };
-              seriesOptions.push(temp);
-              $('section.loader').hide();
-              createChart(seriesOptions, 1);
-          })
+            };
+            seriesOptions.push(temp);
+            $('section.loader').hide();
+            createChart(seriesOptions, 1);
+        })
         .error(function(data, status, headers, config) {
             console.log('Error from addLine');
         })
@@ -48,7 +49,7 @@ angular.module('markoApp').controller('StocksPortfolioCtrl', function ($rootScop
 
     console.log($rootScope.stocks);
     if ($rootScope.stocks.length > 0) {
-      drawExistingLines();
+        drawExistingLines();
     }
 
     // get autocomplete list items
@@ -66,14 +67,17 @@ angular.module('markoApp').controller('StocksPortfolioCtrl', function ($rootScop
     // event listeners
     // add stock
     $rootScope.$on('stockAdd', function(event, data) {
-      $('section.loader').show();
-      addLine(data);
+        if ($rootScope.stocks.length == 1) {
+            $('section.nothing-to-load').hide();
+        }
+        $('section.loader').show();
+        addLine(data);
     });
 
     // remove stock
     $rootScope.$on('stockRemove', function(event, index) {
-      seriesOptions.splice(index, 1);
-      createChart(seriesOptions);
+        seriesOptions.splice(index, 1);
+        createChart(seriesOptions);
     });
 
     $scope.addTicker = function(item) {
