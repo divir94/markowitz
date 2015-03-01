51ea5ad30c3e45fb31e4218bcb7c275a2b31e731
Jon Rovira <jonathanrovira2016@u.northwestern.edu>
2015-02-28 21:56:41 -0600

    cleaned main graph and loader, still need to clean drawer

--- app/scripts/controllers/comparisonController.js
+++ app/scripts/controllers/comparisonController.js
@@ -1,36 +1,29 @@
 angular.module('markoApp').controller('ComparisonCtrl', function ($rootScope, $scope, $route, $http) {
-  $rootScope.activeTab = $route.current.activeTab;
-  setHeights();
-  var seriesOptions = [],
-    seriesCounter = 0;
-  var colors = ['#7cb5ec', '#434348', '#90ed7d', '#f7a35c', '#8085e9', '#f15c80', '#e4d354', '#8085e8', '#8d4653', '#91e8e1'];
-  var names = $rootScope.selectedTickers;
-  console.log(names);
+    $rootScope.activeTab = $route.current.activeTab;
+    setHeights();
+    
+    var seriesOptions = [],
+        seriesCounter = 0;
 
+    angular.element(document).ready(function () {
+        var names = ['AAPL', 'GOOGL'];
 
-  $http.get('https://sleepy-cove-7513.herokuapp.com/quandl', {
-    params: {
-      stocks: ['AAPL', 'IBM']
-    }
-  })
-    .success(function(data, status, headers, config) {
-      console.log(data);
-      names.push('Portfolio');
-      var seriesOptions = [];
-      for (var i = 0; i < data.length; i++) {
-        var stock = data[i];
-        var temp = {
-          name: names[i],
-          color: colors[i%10],
-          data: stock.map(function(obj) {
-            return [ Date.parse(obj[0]), obj[1]];
-          })
-        };
-        seriesOptions.push(temp);
-        createChart(seriesOptions);
-      }
-    })
-    .error(function(data, status, headers, config) {
-      console.log("Error occurred");
+        $.each(names, function (i, name) {
+            $.getJSON('https://www.quandl.com/api/v1/datasets/WIKI/'+name+'.json?exclude_headers=true&sort_order=asc&column=11&collapse=daily&auth_token=J7hxBtcABx8AsszfDzq-', function (result) {
+                seriesOptions[i] = {
+                    name: name,
+                    data: result.data.map(function(obj) {
+                        return [ Date.parse(obj[0]), obj[1]];
+                    })
+                };
+            })
+            .done(function() {
+                seriesCounter += 1;
+                if (seriesCounter == names.length) {
+                    $('section.loader').remove();
+                    createChart(seriesOptions);
+                }
+            });
+        });
     });
 });
