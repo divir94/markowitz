57162d1a3ad26f0b3d06e01a87b738e71f040119
divir94 <divir94@gmail.com>
2015-03-01 11:38:22 -0600

    interactive chord layout

--- app/scripts/controllers/correlationController.js
+++ app/scripts/controllers/correlationController.js
@@ -1,18 +1,16 @@
 angular.module('markoApp').controller('CorrelationCtrl', function ($rootScope, $scope, $http) {
-    test = ['AAPL', 'IBM', 'UPS'];
-    test2 = ['Mkt-RF', 'RF', 'SMB', 'HML']
-    //$rootScope.allStrategies
+    names = ['Mkt-RF', 'RF', 'SMB', 'HML']
 
     // get correlation matrix
     //$http.get('https://sleepy-cove-7513.herokuapp.com/compute/stock', {
     //  params: {
-    //    stocks: test
+    //    stocks: names
     //  }})
     //  .success(function(data, status, headers, config) {
     //    $rootScope.corrMatrix = data;
     //    console.log(data.toString());
     //    var result = {
-    //      packageNames: test,
+    //      packageNames: names,
     //      matrix: data
     //    };
     //
@@ -26,23 +24,27 @@ angular.module('markoApp').controller('CorrelationCtrl', function ($rootScope, $
     //  });
 
     //get correlation matrix
-    $http.get('https://sleepy-cove-7513.herokuapp.com/compute/strategy', {
-      params: {
-        strategies: test2
-      }})
-      .success(function(data, status, headers, config) {
-        $rootScope.corrMatrix = data;
-        var result = {
-          packageNames: test2,
-          matrix: data
-        };
+    $rootScope.$watch('stocks', function (newValue, oldValue) {
+      console.log(oldValue.length, newValue.length);
+      if (newValue !== oldValue)
+        $http.get('https://sleepy-cove-7513.herokuapp.com/compute/strategy', {
+          params: {
+            strategies: newValue
+          }})
+          .success(function(data, status, headers, config) {
+            $rootScope.corrMatrix = data;
+            var result = {
+              packageNames: newValue,
+              matrix: data
+            };
 
-        var chart = d3.chart.dependencyWheel();
-        d3.select('#chart_placeholder')
-          .datum(result)
-          .call(chart);
-      })
-      .error(function(data, status, headers, config) {
-        console.log('Error occurred, data given was ' + data);
-      });
-})
+            var chart = d3.chart.dependencyWheel();
+            d3.select('#chart_placeholder')
+              .datum(result)
+              .call(chart);
+          })
+          .error(function(data, status, headers, config) {
+            console.log('Error occurred, data given was ' + data);
+          });
+    }, true);  // <- put `true` here
+});
